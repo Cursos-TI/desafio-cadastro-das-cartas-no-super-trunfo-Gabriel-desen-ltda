400fdad5c41e9af399e3f813c4907fe19d644652
Gabriel-desen-ltda <gabriel.engenheiro132005@gmail.com>
2025-02-13 01:00:37 +0000

    3.0

--- jogosupertrunfo.c
+++ jogosupertrunfo.c
@@ -7,7 +7,8 @@ int País, estado, cidade, turismo;
 float Pib, área, população;
 char nomes [30];
 
-printf("País: Brasil\n, Estado: SP\n, Cidade: São paulo\n");
+printf("País: %SBrasil\n, Estado: %sSP\n, Cidade: %SSão paulo\n");
+printf("País: %sEstados Unidos\n, Estado: %sNY\n");
 
 
 
